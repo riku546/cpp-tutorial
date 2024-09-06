8e25e48e19451ca0cd9fefcf2b137ca1bf4e0a07
riku <opa9788@gmail.com>
2024-09-06 22:43:09 +0900

    full search

--- parctice/bit_full_search.cpp
+++ parctice/bit_full_search.cpp
@@ -4,42 +4,51 @@ using ull = unsigned long long;
 using ll = long long;
 using namespace std;
 
-
-
-int main(){
-  int N , M;
+int main()
+{
+  int N, M;
   cin >> N >> M;
-  vector<vector<int>>k(M);
-  vector<int>P(M);
-  rep(i , 0 , M) {
-    int km ;
+  vector<vector<int>> k(M);
+  vector<int> P(M);
+  rep(i, 0, M)
+  {
+    int km;
     cin >> km;
-    rep(j , 0 , km) {
+    rep(j, 0, km)
+    {
       int temp;
       cin >> temp;
       k[i].push_back(temp);
     }
   };
-  rep(i , 0 , M) cin >> P[i];
-int ans = 0;
- rep(i , 0 , 1 << N){
-  bool flag = true;
-  vector<int>temp = {};
-  rep(j , 0 , N) {
-    if(i & (1 << j)) {
-      temp.push_back(j);
+  rep(i, 0, M) cin >> P[i];
+  int ans = 0;
+  rep(i, 0, 1 << N)
+  {
+    bool flag = true;
+    vector<int> temp = {};
+    rep(j, 0, N)
+    {
+      if (i & (1 << j))
+      {
+        temp.push_back(j);
       }
     }
-    rep(m , 0 , M){
+    rep(m, 0, M)
+    {
       int cnt = 0;
-      rep(n , 0 ,k[m].size()){
-        if(count(temp.begin() , temp.end() , n)) cnt++;
+      rep(n, 0, k[m].size())
+      {
+        if (count(temp.begin(), temp.end(), n))
+          cnt++;
       }
-      if(cnt%2!=P[m]) flag = false;
+      if (cnt % 2 != P[m])
+        flag = false;
     }
 
-    if(flag ) ans++;
+    if (flag)
+      ans++;
   }
 
   cout << ans << endl;
- }
+}
