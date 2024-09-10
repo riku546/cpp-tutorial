9d35d67df764802cd9526d1472ce5ce90e8150fe
riku546 <opa9788@gmail.com>
2024-09-11 01:16:03 +0900

    tle

--- atcoder_problems/practice/105.cpp
+++ atcoder_problems/practice/105.cpp
@@ -1,3 +1,24 @@
+// #include <bits/stdc++.h>
+// #define rep(i, m, n) for (int i = m; i < n; i++)
+// using ull = unsigned long long;
+// using ll = long long;
+// using namespace std;
+
+// int main()
+// {
+//   int N;
+//   cin >> N;
+//   vector<int> A = {};
+//   rep(i, 1, N + 1) A.push_back(i);
+
+//   do
+//   {
+//     rep(i, 0, N - 1) cout << A[i] << ' ';
+//     cout << endl;
+//   } while (next_permutation(A.begin(), A.end()));
+
+// }
+
 #include <bits/stdc++.h>
 #define rep(i, m, n) for (int i = m; i < n; i++)
 using ull = unsigned long long;
@@ -6,15 +27,11 @@ using namespace std;
 
 int main()
 {
-  int N;
-  cin >> N;
-  vector<int> A = {};
-  rep(i, 1, N + 1) A.push_back(i);
-
-  do
+  rep(i, 1, 10)
   {
-    rep(i, 0, N - 1) cout << A[i] << ' ';
-    cout << endl;
-  } while (next_permutation(A.begin(), A.end()));
-
+    rep(j, 1, 10)
+    {
+      cout << i << ' ' << j << endl;
+    }
+  }
 }
