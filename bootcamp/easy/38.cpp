a27f87a2105b1c7d5eeec587dbd989f689859aa2
riku546 <opa9788@gmail.com>
2024-09-12 05:30:16 +0900

    five dishes

--- bootcamp/easy/38.cpp
+++ bootcamp/easy/38.cpp
@@ -4,8 +4,40 @@ using ull = unsigned long long;
 using ll = long long;
 using namespace std;
 
+int main()
+{
+  int A, B, C, D, E;
+  vector<pair<int, int>> memory(5);
+  int temp = 0;
+  int index;
+  rep(i, 0, memory.size()) cin >> memory[i].first;
 
+  rep(i, 0, memory.size())
+  {
+    int t = 10 - (memory[i].first % 10);
+    if (t == 10)
+    {
+      memory[i].second = 0;
+    }
+    else
+    {
+      memory[i].second = t;
+      if (temp <= t)
+      {
+        temp = t;
+        index = i;
+      }
+    }
+  }
+  int ans = memory[index].first;
 
-int main(){
-  
+  rep(i, 0, memory.size())
+  {
+    if (index == i)
+      continue;
+    ans += memory[i].first;
+    ans += memory[i].second;
+  }
+
+  cout << ans << endl;
 }
