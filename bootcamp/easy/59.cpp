bcf5796d54e15618db4d3e3d9b39853d3ca729fb
riku546 <opa9788@gmail.com>
2024-09-14 09:01:17 +0900

    bootcamp

--- bootcamp/easy/59.cpp
+++ bootcamp/easy/59.cpp
@@ -4,8 +4,26 @@ using ull = unsigned long long;
 using ll = long long;
 using namespace std;
 
+int main()
+{
+  int N;
+  cin >> N;
+  vector<int> a(N);
+  rep(i, 0, N) cin >> a[i];
+  int cnt = 0;
+  int ans;
+  int index = 1;
+  rep(i, 0, N)
+  {
+    if (a[i] == index)
+    {
+      cnt++;
+      index++;
+    }
+  }
 
-
-int main(){
-  
+  if (cnt == 0)
+    cout << -1 << endl;
+  else
+    cout << N - cnt << endl;
 }
