6e2d2efef294188901b80883bf18c394b18e1b3f
riku546 <opa9788@gmail.com>
2024-09-26 07:55:33 +0900

    c

--- c_practice/297.cpp
+++ c_practice/297.cpp
@@ -1,3 +1,35 @@
+// #include <bits/stdc++.h>
+// #define rep(i, m, n) for (int i = m; i < n; i++)
+// using ull = unsigned long long;
+// using ll = long long;
+// using namespace std;
+
+// int main()
+// {
+//   string S = "wbwbwwbwbwbw";
+//   int W, B;
+//   cin >> W >> B;
+//   rep(i, 0, S.size())
+//   {
+//     int w_cnt = 0, b_cnt = 0;
+
+//     rep(j, i, i + W + B)
+//     {
+//       if (S[j % 12] == 'w')
+//         w_cnt++;
+//       else
+//         b_cnt++;
+//     }
+//     if (w_cnt == W && b_cnt == B)
+//     {
+//       cout << "Yes" << endl;
+//       return 0;
+//     }
+//   }
+
+//   cout << "No" << endl;
+// }
+
 #include <bits/stdc++.h>
 #define rep(i, m, n) for (int i = m; i < n; i++)
 using ull = unsigned long long;
@@ -6,27 +38,28 @@ using namespace std;
 
 int main()
 {
-  string S = "wbwbwwbwbwbw";
-  int W, B;
-  cin >> W >> B;
-  rep(i, 0, S.size())
+  int H, W;
+  cin >> H >> W;
+  vector<vector<char>> S(H, vector<char>(W));
+  rep(h, 0, H) rep(w, 0, W) cin >> S[h][w];
+
+  rep(h, 0, H)
   {
-    int w_cnt = 0, b_cnt = 0;
-
-
-    rep(j, i, i + W + B)
+    rep(w, 0, W - 1)
     {
-      if (S[j % 12] == 'w')
-        w_cnt++;
-      else 
-        b_cnt++;
-    }
-    if (w_cnt == W && b_cnt == B)
-    {
-      cout << "Yes" << endl;
-      return 0;
+      if (S[h][w] == 'T' && S[h][w + 1] == 'T')
+      {
+        
+        S[h][w] = 'P';
+        S[h][w + 1] = 'C';
+        w++;
+      }
     }
   }
 
-  cout << "No" << endl;
+  rep(h, 0, H)
+  {
+    rep(w, 0, W) cout << S[h][w];
+    cout << endl;
+  }
 }
