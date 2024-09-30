019943046195beaff1ae1d65abf1fd0e6350b7c9
riku546 <opa9788@gmail.com>
2024-09-30 20:52:30 +0900

    c

--- c_practice/328.cpp
+++ c_practice/328.cpp
@@ -4,13 +4,24 @@ using ull = unsigned long long;
 using ll = long long;
 using namespace std;
 
-
-
-int main(){
-  int N , Q;
+int main()
+{
+  int N, Q;
   string S;
   cin >> N >> Q >> S;
-  
 
+  vector<int> a(N - 1);
+  rep(i, 0, N - 1) a[i] = (S[i] == S[i + 1]);
+  vector<int> sum(N);
+  rep(i, 0, N - 1) sum[i + 1] = sum[i] + a[i];
 
+  rep(i, 0, Q)
+  {
+    int l, r;
+    cin >> l >> r;
+    l--;
+    r--;
+    int ans = sum[r] - sum[l];
+    cout << ans << '\n';
+  }
 }
