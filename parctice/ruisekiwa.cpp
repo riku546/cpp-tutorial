44625fd5e5246ab08d199981f400ce78fb0f5673
riku546 <opa9788@gmail.com>
2024-10-03 09:54:00 +0900

    c

--- parctice/ruisekiwa.cpp
+++ parctice/ruisekiwa.cpp
@@ -26,6 +26,38 @@
 //   cout << ans << endl;
 // }
 
+// #include <bits/stdc++.h>
+// #define rep(i, m, n) for (int i = m; i < n; i++)
+// using ull = unsigned long long;
+// using ll = long long;
+// using namespace std;
+
+// int main()
+// {
+//   int N;
+//   cin >> N;
+//   vector<int> A(N);
+//   vector<ll> memory(N + 1);
+
+//   rep(i, 0, N)
+//   {
+//     int a;
+//     cin >> a;
+//     A[i] = a;
+//     memory[i + 1] = (ll)memory[i] + a;
+//   }
+
+//   rep(i, 1, N + 1)
+//   {
+//     ll ans = 0;
+//     rep(j, 0, memory.size() - i)
+//     {
+//       ans = max(ans, memory[j + i] - memory[j]);
+//     }
+//     cout << ans << endl;
+//   }
+// }
+
 #include <bits/stdc++.h>
 #define rep(i, m, n) for (int i = m; i < n; i++)
 using ull = unsigned long long;
@@ -36,24 +68,18 @@ int main()
 {
   int N;
   cin >> N;
-  vector<int> A(N);
-  vector<ll> memory(N + 1);
-
+  vector<ll> sum(N + 1);
   rep(i, 0, N)
   {
     int a;
     cin >> a;
-    A[i] = a;
-    memory[i + 1] = (ll)memory[i] + a;
+    sum[i + 1] = (ll)sum[i] + a;
   }
 
-  rep(i, 1, N + 1)
+  rep(k, 1, N + 1)
   {
     ll ans = 0;
-    rep(j, 0, memory.size() - i)
-    {
-      ans = max(ans, memory[j + i] - memory[j]);
-    }
+    rep(j, 0, sum.size() - k) ans = max(ans, (ll)sum[j + k] - sum[j]);
     cout << ans << endl;
   }
 }
