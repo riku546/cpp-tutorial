3f24212530393a714cb058995d0ead37095d2cd4
riku <opa9788@gmail.com>
2024-10-03 15:45:22 +0900

    c

--- parctice/ruisekiwa.cpp
+++ parctice/ruisekiwa.cpp
@@ -58,6 +58,31 @@
 //   }
 // }
 
+// #include <bits/stdc++.h>
+// #define rep(i, m, n) for (int i = m; i < n; i++)
+// using ull = unsigned long long;
+// using ll = long long;
+// using namespace std;
+
+// int main()
+// {
+//   int N, Q;
+//   string S;
+//   cin >> N >> Q >> S;
+//   vector<int> memory(N - 1);
+//   vector<ll> sum(N);
+//   rep(i, 0, N - 1) memory[i] = (S[i] == S[i + 1]);
+//   rep(i, 0, N - 1) sum[i + 1] = sum[i] + memory[i];
+
+//   rep(i, 0, Q)
+//   {
+//     int l, r;
+//     cin >> l >> r;
+//     l--, r--;
+//     cout << sum[r] - sum[l] << endl;
+//   }
+// }
+
 #include <bits/stdc++.h>
 #define rep(i, m, n) for (int i = m; i < n; i++)
 using ull = unsigned long long;
@@ -66,19 +91,17 @@ using namespace std;
 
 int main()
 {
-  int N, Q;
-  string S;
-  cin >> N >> Q >> S;
-  vector<int> memory(N - 1);
-  vector<ll> sum(N);
-  rep(i, 0, N - 1) memory[i] = (S[i] == S[i + 1]);
-  rep(i, 0, N - 1) sum[i + 1] = sum[i] + memory[i];
-
-  rep(i, 0, Q)
+  int N, K;
+  cin >> N >> K;
+  vector<ll> sum(N + 1);
+  rep(i, 0, N)
   {
-    int l, r;
-    cin >> l >> r;
-    l--, r--;
-    cout << sum[r] - sum[l] << endl;
+    int a;
+    cin >> a;
+    sum[i + 1] = sum[i] + a;
   }
+  rep(i, 0, N + 1) cout << sum[i] << endl;
+  ll ans = 0;
+  rep(i, 0, N - K + 1) ans += sum[i + K] - sum[i];
+  cout << ans << endl;
 }
