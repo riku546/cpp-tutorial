4fd96ab68d5938897f631dd68308f8bdbad3d5f5
riku <opa9788@gmail.com>
2024-10-11 16:14:18 +0900

    再帰ムズイ

--- parctice/saiki.cpp
+++ parctice/saiki.cpp
@@ -1,23 +1,44 @@
+// #include <bits/stdc++.h>
+// #define rep(i, m, n) for (int i = m; i < n; i++)
+// using ull = unsigned long long;
+// using ll = long long;
+// using namespace std;
+
+// int kuku(int n)
+// {
+//   if (n == 0)
+//     return 0;
+
+//   kuku(n - 1);
+
+//   rep(i, 1, 10) cout << i * n << ' ';
+//   cout << endl;
+
+//   return 0;
+// }
+
+// int main()
+// {
+//   kuku(9);
+// }
+
 #include <bits/stdc++.h>
 #define rep(i, m, n) for (int i = m; i < n; i++)
 using ull = unsigned long long;
 using ll = long long;
 using namespace std;
 
-int kuku(int n)
+int fib(int n)
 {
   if (n == 0)
     return 0;
+  else if (n == 1)
+    return 1;
 
-  kuku(n - 1);
-
-  rep(i, 1, 10) cout << i * n << ' ';
-  cout << endl;
-
-  return 0;
+  return fib(n - 1) + fib(n - 2);
 }
 
 int main()
 {
-  kuku(9);
+  cout << fib(9) << endl;
 }
