909a263b78c10215e50509a4844dea2f7e9c8548
riku546 <opa9788@gmail.com>
2024-11-30 09:48:40 +0900

    rle2

--- parctice/rle.cpp
+++ parctice/rle.cpp
@@ -1,3 +1,41 @@
+// #include <bits/stdc++.h>
+// #define rep(i, m, n) for (int i = m; i < n; i++)
+// using ull = unsigned long long;
+// using ll = long long;
+// using namespace std;
+
+// int main()
+// {
+//   int n , k;
+//   string s;
+//   cin >> n >> k >> s;
+//   vector<pair<char, int>> rle = {};
+//   rep(i, 0, s.size())
+//   {
+//     if (rle.size() != 0 && rle.back().first == s[i])
+//     {
+//       rle.back().second++;
+//     }
+//     else
+//     {
+//       rle.push_back(make_pair(s[i], 1));
+//     }
+//   }
+
+//   int one_cnt = 0;
+//   rep(i , 0 , rle.size()){
+//     if(rle[i].first == '1') one_cnt++;
+//     if(one_cnt == k) swap(rle[i] , rle[i - 1]);
+//   }
+
+//   string ans = "";
+//   for(auto var : rle)
+//   {
+//     ans += string(var.second , var.first);
+//   }
+//   cout << ans << endl;
+// }
+
 #include <bits/stdc++.h>
 #define rep(i, m, n) for (int i = m; i < n; i++)
 using ull = unsigned long long;
@@ -6,32 +44,28 @@ using namespace std;
 
 int main()
 {
-  int n , k;
+  int n, k;
+  cin >> n >> k;
   string s;
-  cin >> n >> k >> s;
-  vector<pair<char, int>> rle = {};
-  rep(i, 0, s.size())
-  {
-    if (rle.size() != 0 && rle.back().first == s[i])
-    {
-      rle.back().second++;
-    }
-    else
-    {
-      rle.push_back(make_pair(s[i], 1));
+  cin >> s;
+  vector<pair<char , int>>rle;
+  rep(i , 0 , n){
+    if(rle.size() && rle.back().first == s[i]) {
+      rle.back().second += 1;
+    }else {
+      rle.push_back(make_pair(s[i] , 1));
     }
   }
+
 
   int one_cnt = 0;
-  rep(i , 0 , rle.size()){
-    if(rle[i].first == '1') one_cnt++;
+  rep(i , 0 , rle.size()) {
+    if(rle[i].first == '1')one_cnt++;
     if(one_cnt == k) swap(rle[i] , rle[i - 1]);
   }
-
-  string ans = "";
-  for(auto var : rle)
-  {
-    ans += string(var.second , var.first);
+  string ans ="";
+  rep(i , 0 , rle.size()){
+    ans += string(rle[i].second , rle[i].first);
   }
   cout << ans << endl;
 }
