b36967cde40a024ca401305773df94dfb846326d
riku546 <opa9788@gmail.com>
2024-12-11 00:37:21 +0900

    ff

--- atcoder-daily-training/ff.cpp
+++ atcoder-daily-training/ff.cpp
@@ -4,40 +4,22 @@ using ull = unsigned long long;
 using ll = long long;
 using namespace std;
 
-int main()
-{
-  int n;
-  cin >> n;
-  map<string, bool> temp;
-  vector<pair<int, int>> ans;
-  rep(i, 0, n)
-  {
-    string s;
-    int t;
-    cin >> s >> t;
-    if (!temp[s])
-    {
-      ans.push_back(make_pair(t, i + 1));
-      temp[s] = true;
-    }
-  }
 
-  sort(ans.begin(), ans.end());
-  int best_value = ans.back().first;
-  if (best_value == ans[ans.size() - 2].first)
-  {
-    for (int i = -3; i < 0; i--)
-    {
-      if (best_value != ans[ans.size() + i].first)
-      {
-        cout << ans[ans.size() + i + 1].second << endl;
-        return 0;
-      }
-    }
-  }
-  else
-  {
 
-    cout << ans.back().second << endl;
+int main(){
+  int n , q;
+  cin >> n >> q;
+  set<pair<int , int>>ff;
+
+  rep(i , 0 , q){
+    int t , a , b;
+    cin >> t >> a >> b;
+
+    if(t == 1) ff.insert(make_pair(a, b));
+    if(t == 2) ff.erase(make_pair(a , b));
+    if(t == 3){
+      if(ff.count(make_pair(a , b)) && ff.count(make_pair(b , a))) cout << "Yes" << endl;
+      else cout << "No" << endl;
+    }
   }
 }
